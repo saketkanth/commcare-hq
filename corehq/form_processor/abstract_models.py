638b126824f62c05275bba9a8f841fa5c4fdaa5d
NoahCarnahan <ncarnahan@dimagi.com>
2016-01-01 09:29:04 -0500

    Move shared sync log logic from couch implementation to abstract model

--- corehq/form_processor/abstract_models.py
+++ corehq/form_processor/abstract_models.py
@@ -268,6 +268,148 @@ class AbstractSyncLog(object):
     def to_json(self):
         raise NotImplementedError
 
+    def purge_dependent_cases(self):
+        """
+        Attempt to purge any dependent cases from the sync log.
+        """
+        # this is done when migrating from old formats or during initial sync
+        # to purge non-relevant dependencies
+        for dependent_case_id in list(self.dependent_case_ids_on_phone):
+            # need this additional check since the case might have already been purged/remove
+            # as a result of purging the child case
+            if dependent_case_id in self.dependent_case_ids_on_phone:
+                # this will be a no-op if the case cannot be purged due to dependencies
+                self.purge(dependent_case_id)
+
+    def purge(self, case_id):
+        """
+        This happens in 3 phases, and recursively tries to purge outgoing indices of purged cases.
+        Definitions:
+        -----------
+        A case is *relevant* if:
+        - it is open and owned or,
+        - it has a relevant child or,
+        - it has a relevant extension or,
+        - it is the extension of a relevant case.
+
+        A case is *available* if:
+        - it is open and not an extension case or,
+        - it is open and is the extension of an available case.
+
+        A case is *live* if:
+        - it is owned and available or,
+        - it has a live child or,
+        - it has a live extension or,
+        - it is the exension of a live case.
+
+        Algorithm:
+        ----------
+        1. Mark *relevant* cases
+            Mark all open cases owned by the user relevant. Traversing all outgoing child
+            and extension indexes, as well as all incoming extension indexes, mark all
+            touched cases relevant.
+
+        2. Mark *available* cases
+            Mark all relevant cases that are open and have no outgoing extension indexes
+            as available. Traverse incoming extension indexes which don't lead to closed
+            cases, mark all touched cases as available.
+
+        3. Mark *live* cases
+            Mark all relevant, owned, available cases as live. Traverse incoming
+            extension indexes which don't lead to closed cases, mark all touched
+            cases as live.
+        """
+        logger.debug("purging: {}".format(case_id))
+        self.dependent_case_ids_on_phone.add(case_id)
+        relevant = self._get_relevant_cases(case_id)
+        available = self._get_available_cases(relevant)
+        live = self._get_live_cases(available)
+        to_remove = relevant - live
+        self._remove_cases_purge_indices(to_remove, case_id)
+
+    def _get_relevant_cases(self, case_id):
+        """
+        Mark all open cases owned by the user relevant. Traversing all outgoing child
+        and extension indexes, as well as all incoming extension indexes,
+        mark all touched cases relevant.
+        """
+        from casexml.apps.phone.models import IndexTree
+        relevant = IndexTree.get_all_dependencies(
+            case_id,
+            closed_cases=self.closed_cases,
+            child_index_tree=self.index_tree,
+            extension_index_tree=self.extension_index_tree,
+            cached_child_map=_reverse_index_map(self.index_tree.indices),
+            cached_extension_map=_reverse_index_map(self.extension_index_tree.indices),
+        )
+        logger.debug("Relevant cases: {}".format(relevant))
+
+        return relevant
+
+    def _get_available_cases(self, relevant):
+        """
+        Mark all relevant cases that are open and have no outgoing extension indexes
+        as available. Traverse incoming extension indexes which don't lead to closed
+        cases, mark all touched cases as available
+        """
+        incoming_extensions = _reverse_index_map(self.extension_index_tree.indices)
+        available = {case for case in relevant
+                     if case not in self.closed_cases
+                     and not self.extension_index_tree.indices.get(case) or self.index_tree.indices.get(case)}
+        new_available = set() | available
+        while new_available:
+            case_to_check = new_available.pop()
+            for incoming_extension in incoming_extensions.get(case_to_check, []):
+                if incoming_extension not in self.closed_cases:
+                    new_available.add(incoming_extension)
+            available = available | new_available
+        logger.debug("Available cases: {}".format(available))
+
+        return available
+
+    def _get_live_cases(self, available):
+        """
+        Mark all relevant, owned, available cases as live. Traverse incoming
+        extension indexes which don't lead to closed cases, mark all touched
+        cases as available.
+        """
+        from casexml.apps.phone.models import IndexTree
+        incoming_extensions = _reverse_index_map(self.extension_index_tree.indices)
+        live = {case for case in available if case in self.primary_case_ids}
+        new_live = set() | live
+        checked = set()
+        while new_live:
+            case_to_check = new_live.pop()
+            checked.add(case_to_check)
+            new_live = new_live | IndexTree.get_all_outgoing_cases(
+                case_to_check,
+                self.index_tree,
+                self.extension_index_tree
+            )
+            new_live = new_live | IndexTree.traverse_incoming_extensions(
+                case_to_check,
+                self.extension_index_tree,
+                self.closed_cases, cached_map=incoming_extensions
+            )
+            new_live = new_live - checked
+            live = live | new_live
+
+        logger.debug("live cases: {}".format(live))
+
+        return live
+
+    def _remove_cases_purge_indices(self, all_to_remove, checked_case_id):
+        """Remove all cases marked for removal. Traverse child cases and try to purge those too."""
+        logger.debug("cases to to_remove: {}".format(all_to_remove))
+        for to_remove in all_to_remove:
+            indices = self.index_tree.indices.get(to_remove, {})
+            self._remove_case(to_remove, all_to_remove, checked_case_id)
+            for referenced_case in indices.values():
+                is_dependent_case = referenced_case in self.dependent_case_ids_on_phone
+                already_primed_for_removal = referenced_case in all_to_remove
+                if is_dependent_case and not already_primed_for_removal and referenced_case != checked_case_id:
+                    self.purge(referenced_case)
+
 
 class AbstractLedgerValue(six.with_metaclass(ABCMeta)):
     @abstractproperty
@@ -297,3 +439,11 @@ class AbstractSupplyInterface(six.with_metaclass(ABCMeta)):
     @abstractmethod
     def get_or_create_by_location(cls, location):
         raise NotImplementedError
+
+
+def _reverse_index_map(index_map):
+    reverse_indices = defaultdict(set)
+    for case_id, indices in index_map.items():
+        for indexed_case_id in indices.values():
+            reverse_indices[indexed_case_id].add(case_id)
+    return dict(reverse_indices)
